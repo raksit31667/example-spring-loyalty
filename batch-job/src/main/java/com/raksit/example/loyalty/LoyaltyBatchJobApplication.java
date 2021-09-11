b00e62a5ccb4a8d208b356fd99668da546162eb3
raksit31667 <raksit.m@ku.th>
2021-09-11 14:05:17 +0700

    :construction: create a batch-job subproject with default cross-cutting concern dependencies

---
+++ batch-job/src/main/java/com/raksit/example/loyalty/LoyaltyBatchJobApplication.java
@@ -0,0 +1,12 @@
+package com.raksit.example.loyalty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoyaltyBatchJobApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LoyaltyBatchJobApplication.class, args);
+  }
+}
---
+++ batch-job/src/main/resources/db/migration/V1__create_user.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+  id uuid DEFAULT gen_random_uuid() PRIMARY KEY,
+  first_name VARCHAR NOT NULL,
+  last_name VARCHAR NOT NULL,
+  email VARCHAR NOT NULL,
+  phone VARCHAR NOT NULL
+);
