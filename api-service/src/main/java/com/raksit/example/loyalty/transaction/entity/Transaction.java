a68b33ce101ab372d3b3c407c1497d74284c223c
raksit31667 <raksit.m@ku.th>
2021-10-28 19:58:43 +0700

    :necktie: add creation timestamp into Transaction

--- api-service/src/main/java/com/raksit/example/loyalty/transaction/entity/Transaction.java
+++ api-service/src/main/java/com/raksit/example/loyalty/transaction/entity/Transaction.java
@@ -4,6 +4,7 @@ import com.raksit.example.loyalty.user.User;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.Instant;
 import java.util.UUID;
@@ -28,6 +29,9 @@ public class Transaction {
   private Long points;
 
   private Instant performedOn;
+
+  @CreationTimestamp
+  private Instant createdAt;
 
   public Transaction(User user, Long points, Instant performedOn) {
     this.user = user;
--- api-service/src/main/resources/db/migration/V20211011140007__create_transaction.sql
+++ api-service/src/main/resources/db/migration/V20211011140007__create_transaction.sql
@@ -3,5 +3,6 @@ CREATE TABLE transaction
     id           UUID DEFAULT gen_random_uuid() PRIMARY KEY,
     user_id      UUID        NOT NULL REFERENCES users,
     points       BIGINT      NOT NULL,
-    performed_on TIMESTAMPTZ NOT NULL
+    performed_on TIMESTAMPTZ NOT NULL,
+    created_at   TIMESTAMPTZ NOT NULL
 );
