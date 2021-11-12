5160010076ffb3588ae1aa5f68851b6cf9c6f2f5
raksit31667 <raksit.m@ku.th>
2021-11-12 22:00:16 +0700

    :wrench: make batch-job exit after it launches migrate legacy loyalty job

--- batch-job/src/main/java/com/raksit/example/loyalty/LoyaltyBatchJobApplication.java
+++ batch-job/src/main/java/com/raksit/example/loyalty/LoyaltyBatchJobApplication.java
@@ -11,6 +11,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class LoyaltyBatchJobApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(LoyaltyBatchJobApplication.class, args);
+    System.exit(SpringApplication.exit(SpringApplication.run(LoyaltyBatchJobApplication.class, args)));
   }
 }
