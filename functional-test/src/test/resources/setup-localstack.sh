03d397a381c34d079aa2757591e6dadee8c0e28a
raksit31667 <raksit.m@ku.th>
2022-03-20 00:47:02 +0700

    🐳 create a Docker Compose for api-service functional testing
    It depends on PostgrSQL and Localstack

---
+++ functional-test/src/test/resources/setup-localstack.sh
@@ -0,0 +1,18 @@
+awslocal kinesis create-stream --stream-name activity-performed --shard-count 1
+
+awslocal dynamodb create-table \
+--table-name example-spring-loyalty-lock \
+--attribute-definitions AttributeName=lockKey,AttributeType=S AttributeName=sortKey,AttributeType=S \
+--key-schema AttributeName=lockKey,KeyType=HASH AttributeName=sortKey,KeyType=RANGE \
+--provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 \
+--tags Key=Owner,Value=localstack
+
+awslocal dynamodb create-table \
+--table-name example-spring-loyalty-checkpoint \
+--attribute-definitions AttributeName=KEY,AttributeType=S \
+--key-schema AttributeName=KEY,KeyType=HASH \
+--provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 \
+--tags Key=Owner,Value=localstack
+
+awslocal dynamodb list-tables
+awslocal kinesis list-streams
