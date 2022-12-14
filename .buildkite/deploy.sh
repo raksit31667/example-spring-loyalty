9b193236e63f748e6dc2dbfae0be58c8cdc2214f
raksit31667 <raksit.m@ku.th>
2022-12-14 16:26:10 +0700

    :hammer: update deployment script to receive service name from argument

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -1,7 +1,13 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+service_name="$1"
+
 wget https://get.helm.sh/helm-v3.8.2-linux-amd64.tar.gz
 tar -zxvf helm-v3.8.2-linux-amd64.tar.gz
 ./linux-amd64/helm version
 
 aws eks --region ap-southeast-1 update-kubeconfig --name eks-raksit31667
 
-./linux-amd64/helm upgrade --install example-spring-loyalty-api-service ./api-service/helm --set image.tag=${SEMANTIC_VERSION}
+./linux-amd64/helm upgrade --install example-spring-loyalty-${service_name} ./${service_name}/helm --set image.tag=${SEMANTIC_VERSION}
