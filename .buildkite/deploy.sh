906d795b0fbdc9486a483baee68cba05b91c5eae
raksit31667 <raksit.m@ku.th>
2022-12-14 15:03:03 +0700

    :construction_worker: install Helm binary directly instead of get_helm.sh
    [sudo] password for buildkite-agent

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -1,7 +1,7 @@
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-chmod 700 get_helm.sh
-./get_helm.sh
+wget https://get.helm.sh/helm-v3.9.0-linux-amd64.tar.gz
+tar -zxvf helm-v3.9.0-linux-amd64.tar.gz
+./linux-amd64/helm version
 
 aws eks --region ap-southeast-1 update-kubeconfig --name eks-raksit31667
 
-helm upgrade --install example-spring-loyalty-api-service ./api-service/helm --set image.tag=${SEMANTIC_VERSION}
+./linux-amd64/helm upgrade --install example-spring-loyalty-api-service ./api-service/helm --set image.tag=${SEMANTIC_VERSION}
